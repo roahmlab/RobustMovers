80a76fefc3fb064dc0484b23e7d290fcade8c27a
Bohao Zhang <41474684+Cfather@users.noreply.github.com>
2025-05-10 15:47:39 -0400

    add torque exceed action option

--- Examples/Kinova/JointSpaceControl/kinova_controller_armour_pybindwrapper.cpp
+++ Examples/Kinova/JointSpaceControl/kinova_controller_armour_pybindwrapper.cpp
@@ -9,7 +9,8 @@ kinova_controller_armour_pybindwrapper::kinova_controller_armour_pybindwrapper(
     const nb_1d_double& Kr,
     const double V_max,
     const double alpha,
-    const double r_norm_threshold) :
+    const double r_norm_threshold,
+    const std::string exceed_torque_action) :
     urdf_filename_copy(urdf_filename) {
     modelPtr_ = std::make_shared<model>(
         urdf_filename, config_filename);
@@ -52,6 +53,16 @@ kinova_controller_armour_pybindwrapper::kinova_controller_armour_pybindwrapper(
     qd_dd = VecX::Zero(modelPtr_->NB);
 
     tau = VecX::Zero(modelPtr_->NB);
+
+    if (exceed_torque_action == "clamp") {
+        exceed_torque_action_ = EXCEED_TORQUE_ACTION::CLAMP;
+    }
+    else if (exceed_torque_action == "ignore") {
+        exceed_torque_action_ = EXCEED_TORQUE_ACTION::IGNORE;
+    }
+    else {
+        exceed_torque_action_ = EXCEED_TORQUE_ACTION::ERROR;
+    }
 }
 
 void kinova_controller_armour_pybindwrapper::reset_model_parameters(const std::string config_filename) {
@@ -147,19 +158,23 @@ nb::ndarray<nb::numpy, const double> kinova_controller_armour_pybindwrapper::upd
 
     tau = controllerPtr_->update(q, q_d, qd, qd_d, qd_dd);
 
-    for (int i = 0; i < modelPtr_->NB; i++) {
-        if (tau(i) > TORQUE_LIMITS_UPPER[i] ||
-            tau(i) < TORQUE_LIMITS_LOWER[i]) {
-            // std::cerr << "motor: " << i << " tau: " << tau(i) << std::endl;
-            // std::cerr << "limits: [" << TORQUE_LIMITS_LOWER[i] << ", " << TORQUE_LIMITS_UPPER[i] << "]" << std::endl; 
-            // throw std::runtime_error("kinova_controller_armour_pybindwrapper.cpp: update(): torque limits exceeded!");
+    if (exceed_torque_action_ != EXCEED_TORQUE_ACTION::IGNORE) {
+        for (int i = 0; i < modelPtr_->NB; i++) {
+            if (tau(i) > TORQUE_LIMITS_UPPER[i] ||
+                tau(i) < TORQUE_LIMITS_LOWER[i]) {
+                if (exceed_torque_action_ == EXCEED_TORQUE_ACTION::ERROR) {
+                    std::cerr << "motor: " << i << " tau: " << tau(i) << std::endl;
+                    std::cerr << "limits: [" << TORQUE_LIMITS_LOWER[i] << ", " << TORQUE_LIMITS_UPPER[i] << "]" << std::endl; 
+                    throw std::runtime_error("kinova_controller_armour_pybindwrapper.cpp: update(): torque limits exceeded!");
+                }
 
-            // clamp tau
-            if (tau(i) > TORQUE_LIMITS_UPPER[i]) {
-                tau(i) = TORQUE_LIMITS_UPPER[i];
-            }
-            else if (tau(i) < TORQUE_LIMITS_LOWER[i]) {
-                tau(i) = TORQUE_LIMITS_LOWER[i];
+                // clamp tau
+                if (tau(i) > TORQUE_LIMITS_UPPER[i]) {
+                    tau(i) = TORQUE_LIMITS_UPPER[i];
+                }
+                else if (tau(i) < TORQUE_LIMITS_LOWER[i]) {
+                    tau(i) = TORQUE_LIMITS_LOWER[i];
+                }
             }
         }
     }
